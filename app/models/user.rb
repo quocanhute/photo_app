7490b53306921f9ca94ee3ece938718634d650a2
Quoc Anh <quocanh10a2@gmail.com>
2023-06-27 10:40:28 +0700

    create model User

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Add the role attribute
+  enum role: { admin: 0, user: 1 }
+end
