1645b6e8a6ddefa5403b9e4c18f0866faf831667
Quoc Anh <quocanh10a2@gmail.com>
2023-07-06 10:38:11 +0700

    done demo card img and album

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,6 +6,14 @@ class ProfileController < ApplicationController
     @photos = @user.photos
   end
 
+  def show_photo
+
+  end
+
+  def show_album
+
+  end
+
   def follow
     Relationship.create_or_find_by(follower_id: current_user.id, followee_id: @user.id)
     redirect_back(fallback_location: root_path)
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,10 @@ Rails.application.routes.draw do
   get '/profile/:id', to: 'profile#show', as: 'view_profile'
   post 'profile/follow/:id', to: 'profile#follow', as: 'profile_follow'
   delete 'profile/unfollow/:id', to: 'profile#unfollow', as: 'profile_unfollow'
+  # ==============
+  get '/profile/:id/photos', to: 'profile#show_photo', as: 'view_photos_user'
+  get '/profile/:id/albums', to: 'profile#show_album', as: 'view_albums_user'
+  # ==============
   # ============================================
   post 'like_photo/:id', to: 'home#like_photo', as: 'like_photo'
   post 'like_album/:id', to: 'home#like_album', as: 'like_album'
