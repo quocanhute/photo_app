141423087ba964ff266cf266e7abd7ded57575a6
Quoc Anh <quocanh10a2@gmail.com>
2023-07-24 23:34:27 +0700

    update responsive index for Android

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ require 'net/http'
 
 def generate_random_image
   # Fetch random image URL from Unsplash API
-  access_key = 'ttmgSgDGmNG6ih6kMmWjeIwdu2jrsEuBdrUxg_E-uZo'
+  # access_key = 'ttmgSgDGmNG6ih6kMmWjeIwdu2jrsEuBdrUxg_E-uZo'
+  access_key = 'y2DOCsMbq4sVkOmC2UC9U4270AIRy2YBTx7vLssYkLQ'
+
   url = URI.parse("https://api.unsplash.com/photos/random?client_id=#{access_key}")
   response = Net::HTTP.get_response(url)
   json = JSON.parse(response.body)
@@ -26,9 +28,24 @@ end
 #               password: password,)
 # end
 
-50.times do
+# 50.times do
+#   user_id = User.pluck(:id).sample
+#   photo = Photo.new(
+#     description: Faker::Lorem.sentence(word_count: 30, supplemental: true, random_words_to_add:2),
+#     title: Faker::Lorem.sentence(word_count: [7,8,9,10,11].sample),
+#     user_id: user_id,
+#     is_public: [true,false].sample
+#   )
+#
+#   file = URI.open(generate_random_image)
+#   photo.img.attach(io: file, filename: "file_name")
+#   photo.save
+#
+# end
+
+25.times do
   user_id = User.pluck(:id).sample
-  photo = Photo.new(
+  album = Album.new(
     description: Faker::Lorem.sentence(word_count: 30, supplemental: true, random_words_to_add:2),
     title: Faker::Lorem.sentence(word_count: [7,8,9,10,11].sample),
     user_id: user_id,
@@ -36,7 +53,8 @@ end
   )
 
   file = URI.open(generate_random_image)
-  photo.img.attach(io: file, filename: "file_name")
-  photo.save
+  file1 = URI.open(generate_random_image)
+  album.images.attach([io: file, filename: "image1"],[io: file1, filename: "image2"])
+  album.save
 
 end
