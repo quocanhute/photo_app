f0dd64443b6ebdacd2bb17e521ad463cd9cf1899
Quoc Anh <quocanh10a2@gmail.com>
2023-10-21 14:21:51 +0700

    Hot fix 1.1.2

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module PhotoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 7.0
+    config.load_defaults 7.0
     config.autoloader = :classic
 
     # Configuration for the application, engines, and railties goes here.
