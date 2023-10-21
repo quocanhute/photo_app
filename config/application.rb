23004192a6184ba10659e981528d2e6145019d45
Quoc Anh <quocanh10a2@gmail.com>
2023-10-21 14:11:23 +0700

    Hot fix 1.1

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,10 @@ Bundler.require(*Rails.groups)
 module PhotoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
+    # config.load_defaults 7.0
+    config.autoloader = :classic
 
     # Configuration for the application, engines, and railties goes here.
-
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}")]
     config.i18n.default_locale = :en
     config.i18n.available_locales = [:en, :vi]
