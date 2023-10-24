4e966b61bf6562165a94a2f6c48e96093417d753
Quoc Anh <quocanh10a2@gmail.com>
2023-10-24 22:01:45 +0700

    update something

--- app/javascript/controllers/edit_element_post_controller.js
+++ app/javascript/controllers/edit_element_post_controller.js
@@ -16,6 +16,7 @@ export default class extends Controller {
 
   clickCancel(event) {
     if (!event.target.matches('.cancel')) return;
+    event.preventDefault()
     let element = event.target.closest('.paragraph-form')
     element.classList.add('d-none')
     element.previousElementSibling.classList.remove('d-none')
